ee4ec9b8be32db161ca1c32a3e41458546e7e1ac
RubenTeles <ruben-teles@hotmail.com>
2022-09-29 11:03:06 +0100

    fix CTRL D

--- srcs/commands/ft_exit.c
+++ srcs/commands/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 22:49:00 by rteles            #+#    #+#             */
-/*   Updated: 2022/09/29 01:02:51 by rteles           ###   ########.fr       */
+/*   Updated: 2022/09/29 11:02:41 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,26 @@ static void	exit_execute(t_command *c, int in)
 		if (in == -1)
 			return ;
 		execute(c, in, 2);
-	}
-	if (c->count_cmd > 2)
-	{
-		printf("cd: too many arguments\n");
-		(terminal())->last_exit = 1;
-		return ;
-	}
-	if (c->count_cmd == 2)
-	{
-		if ((string())->only_this_chars(c->command[1], "0123456789"))
+		if (c->count_cmd > 2)
 		{
-			printf("precisa de ser convertido %s\n", c->command[1]);
-			//x = (string()).atoi(c->command[1]);
-			//(terminal())->last_exit = x;
-		}
-		else
-		{
-			printf("exit: %s: numeric argument required", c->command[1]);
-			(terminal())->last_exit = 2;
+			printf("cd: too many arguments\n");
+			(terminal())->last_exit = 1;
 			return ;
+		}
+		if (c->count_cmd == 2)
+		{
+			if ((string())->only_this_chars(c->command[1], "0123456789"))
+			{
+				printf("precisa de ser convertido %s\n", c->command[1]);
+				//x = (string()).atoi(c->command[1]);
+				//(terminal())->last_exit = x;
+			}
+			else
+			{
+				printf("exit: %s: numeric argument required", c->command[1]);
+				(terminal())->last_exit = 2;
+				return ;
+			}
 		}
 	}
 	write(STDOUT_FILENO, "exit\n", 5);
